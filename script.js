6c10d1a634d3e1a803ebcd24ccd32ac69363323f
Bukya jagadish <91822729+bukya123@users.noreply.github.com>
2025-02-20 16:13:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  arr.sort();
+	let sum=arr[0];
+	int ans=0;
+	for(let i=1;i<arr.length;i++){
+		sum+=arr[i];
+		ans+=sum;
+	}
+	return ans;
 }
 
 module.exports=mincost;
